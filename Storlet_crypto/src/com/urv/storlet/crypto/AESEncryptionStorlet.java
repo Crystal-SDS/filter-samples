bdce90f6bf0bc978dead1d90960fa5160b0aec6d
raulgracia <raulgraciatinedo@gmail.com>
2016-04-21 15:08:00 +0200

    First version for experiments of AES crypto storlet. Still lacks from appropriate key management.

--- Storlet_crypto/src/com/urv/storlet/crypto/AESEncryptionStorlet.java
+++ Storlet_crypto/src/com/urv/storlet/crypto/AESEncryptionStorlet.java
@@ -1,17 +1,29 @@
 package com.urv.storlet.crypto;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
-import com.urv.storlet.sql.IStorlet;
+import com.ibm.storlet.common.IStorlet;
+import com.ibm.storlet.common.StorletException;
+import com.ibm.storlet.common.StorletInputStream;
+import com.ibm.storlet.common.StorletLogger;
+import com.ibm.storlet.common.StorletObjectOutputStream;
+import com.ibm.storlet.common.StorletOutputStream;
 
 /**
  * 
@@ -47,59 +59,42 @@ public class AESEncryptionStorlet implements IStorlet {
 		IvParameterSpec iv = null;
 		SecretKeySpec skeySpec = null;
 		Cipher cipher = null;
-		String key = "getActualKeyFromRedis"; //TODO: Get keys from Redis
+		String key = "getActualKeyFrom"; //TODO: Get keys from Redis, 16 bytes key!
 		try {
 			iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
 			skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-	        cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");        
-		} catch (UnsupportedEncodingException e) {
+	        cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");   
+			/*
+			 * Get reverse flag to decide if to encrypt/decrypt
+			 */
+	        //TODO: Check that the  parameter usage is correct
+			if (parameters.get("reverse") != null && Boolean.parseBoolean(parameters.get("reverse"))) {
+				cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+			}else cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while((len = is.read(buffer)) != -1) {
+				byte[] cryptoContent = cipher.update(buffer);
+				outputStream.write(cryptoContent, 0, cryptoContent.length);
+				//TODO: As AES does padding, we may need to store the original object len on encryption
+			}
+		} catch (IOException e) {
+			log.emitLog("Encryption - raised IOException: " + e.getMessage());
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
 			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		}
-
-		final int ENCRYPT = 0;
-		final int DECRYPT = 1;
-
-		int action = ENCRYPT;
-		
-		/*
-		 * Get optional action flag
-		 */
-        String action_str = parameters.get("action");
-		if (action_str != null && action_str.equals("reverse")) {
-			action = DECRYPT;
-		}
-
-		try {
-			byte[] buffer = new byte[65536];
-			int len;
-			if (action == ENCRYPT) {
-				cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);				
-				while((len=is.read(buffer)) != -1) {
-					byte[] encrypted = cipher.update(buffer);
-					outputStream.write(encrypted, 0, encrypted.length);
-				}
-			} else {
-				cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-				while((len = is.read(buffer)) != -1) {
-					byte[] decrypted = cipher.update(buffer);
-					outputStream.write(decrypted, 0, decrypted.length);
-				}
-			}
-		} catch (IOException e) {
-			log.emitLog("Encryption - raised IOException: " + e.getMessage());
 		} finally {
 			try {
 				is.close();
 				outputStream.close();
 			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		log.emitLog("EncryptionStorlet Invocation done");
