ab400db32e2421836850b05dd490f1dd63f7db64
JosepSampe <jsampe@gmail.com>
2016-06-13 15:39:22 +0200

    New Singleton BW-Dif

--- Native_bandwidth_differentiation/crystal_bandwidth_control.py
+++ Native_bandwidth_differentiation/crystal_bandwidth_control.py
@@ -40,7 +40,8 @@ class BandwidthThreadControl(Thread):
         self.transferred_bytes_control = 0
         '''Number of data flow iterations within control interval'''
         self.number_of_iterations = 0
-                 
+        '''Control periodically the enforcement of bw limits'''
+        #self.control_process = eventlet.spawn(self.rate_control)
         '''Bandwidth limit per thread'''
         self.aggregated_bandwidth_limit = BW_MAX  #Minimum BW for non-QoS tenants
         self.bw_limits = dict()
@@ -59,11 +60,10 @@ class BandwidthThreadControl(Thread):
         self.iters_mean = 1
         self.counter = 0
         
-        '''Control periodically the enforcement of bw limits'''
-        #self.control_process = eventlet.spawn(self.rate_control)
-        self.control_process = Thread(target=self.rate_control)
-        self.control_process.start()
-
+        self.rate_control = Thread(target=self.rate_control)
+        self.rate_control.daemon = True
+        self.rate_control.start()
+        
     def add_stream_to_tenant(self, write_pipe, read_pipe, policy, device):
         pipe_tuple = (write_pipe, read_pipe, policy, device)
         self.stream_pipe_queue.put(pipe_tuple)
@@ -119,7 +119,6 @@ class BandwidthThreadControl(Thread):
         return diff_transferred_data
         
     def rate_control(self):
-
         while self.alive:
             f = open("/home/lab144/control.dat",'a+')
             f.write("----------------\n")
@@ -171,8 +170,8 @@ class BandwidthThreadControl(Thread):
 
             time.sleep(SAMPLE_CONTROL_INTERVAL)
              
-            
     def _read_chunk(self, reader):
+        
         if self.server == 'proxy' and self.method == 'GET':
             try:
                 return reader.next()
@@ -192,7 +191,7 @@ class BandwidthThreadControl(Thread):
             writer.close()
             raise
     
-    def run(self):        
+    def run(self):      
         while self.alive:
             try:
                 stream_data = self.stream_pipe_queue.get(timeout=self.timeout)
@@ -317,7 +316,7 @@ class IterLike(object):
     def read_with_timeout(self, size):
         try:
             with Timeout(self.timeout):
-                chunk = self.obj_data.read(size)
+                chunk = os.read(self.obj_data, size)
         except Timeout:
             self.close()
             raise
@@ -328,9 +327,17 @@ class IterLike(object):
 
     def next(self, size=64 * 1024):
         if len(self.buf) < size:
-            self.buf += self.read_with_timeout(size - len(self.buf))
-            if self.buf == b'':
-                raise StopIteration('Stopped iterator exp')
+            r, w, e = select.select([self.obj_data], [], [], self.timeout)
+            if len(r) == 0:
+                self.close()
+
+            if self.obj_data in r:
+                self.buf += self.read_with_timeout(size - len(self.buf))
+                if self.buf == b'':
+                    self.close()
+                    raise StopIteration('Stopped iterator ex')
+            else:
+                raise StopIteration('Stopped iterator ex')
 
         if len(self.buf) > size:
             data = self.buf[:size]
@@ -392,64 +399,245 @@ class IterLike(object):
         return lines
 
     def close(self):
-        print "---> Closing BW-DIFF Pipe <---"
+        if self.closed:
+            return
+        print "---> Closed BW-DIFF Pipe <---"
+        self.closed = True
+        os.close(self.obj_data)
+
+    def __del__(self):
+        self.close()
+        
+class IterLikePoll(object):
+    def __init__(self, obj_data, timeout):
+        self.closed = False
+        self.obj_data = obj_data
+        self.timeout = timeout
+        self.buf = b''
+
+        self.epoll = select.epoll()
+        self.epoll.register(self.obj_data, select.EPOLLIN | select.EPOLLPRI)
+
+    def __iter__(self):
+        return self
+
+    def read_with_timeout(self, size):
+        try:
+            with Timeout(self.timeout):
+                chunk = os.read(self.obj_data,size)
+        except Timeout:
+            self.close()
+            raise
+        except Exception:
+            self.close()
+            raise
+        return chunk
+
+    def next(self, size=64 * 1024):
+        if len(self.buf) < size:
+            r = self.epoll.poll(self.timeout)
+
+            if len(r) == 0:
+                self.close()
+            elif self.obj_data in r[0]:
+                self.buf += self.read_with_timeout(size - len(self.buf))
+                if self.buf == b'':
+                    raise StopIteration('Stopped iterator exp')
+            else:
+                raise StopIteration('Stopped iterator exi')
+
+        if len(self.buf) > size:
+            data = self.buf[:size]
+            self.buf = self.buf[size:]
+        else:
+            data = self.buf
+            self.buf = b''
+        return data
+
+    def _close_check(self):
+        if self.closed:
+            raise ValueError('I/O operation on closed file')
+
+    def read(self, size=64 * 1024):
+        self._close_check()
+        return self.next(size)
+
+    def readline(self, size=-1):
+        self._close_check()
+
+        # read data into self.buf if there is not enough data
+        while b'\n' not in self.buf and \
+              (size < 0 or len(self.buf) < size):
+            if size < 0:
+                chunk = self.read()
+            else:
+                chunk = self.read(size - len(self.buf))
+            if not chunk:
+                break
+            self.buf += chunk
+
+        # Retrieve one line from buf
+        data, sep, rest = self.buf.partition(b'\n')
+        data += sep
+        self.buf = rest
+
+        # cut out size from retrieved line
+        if size >= 0 and len(data) > size:
+            self.buf = data[size:] + self.buf
+            data = data[:size]
+
+        return data
+
+    def readlines(self, sizehint=-1):
+        self._close_check()
+        lines = []
+        try:
+            while True:
+                line = self.readline(sizehint)
+                if not line:
+                    break
+                lines.append(line)
+                if sizehint >= 0:
+                    sizehint -= len(line)
+                    if sizehint <= 0:
+                        break
+        except StopIteration:
+            pass
+        return lines
+
+    def close(self):
         if self.closed:
             return
         self.closed = True
+        self.epoll.unregister(self.obj_data)
+        self.epoll.close()
+        os.close(self.obj_data)
+
+    def __del__(self):
+        self.close()
+    
+
+class IterLikeOpen(object):
+    def __init__(self, obj_data, timeout):
+        self.closed = False
+        self.obj_data = obj_data
+        self.timeout = timeout
+        self.buf = b''
+        
+        """
+        if hasattr(obj_data, 'read'):
+            self.reader = True
+        else:
+            self.reader = False
+        """   
+
+    def __iter__(self):
+        return self
+
+    def read_with_timeout(self, size):
+        try:
+            with Timeout(self.timeout):
+                #if self.reader:
+                chunk = self.obj_data.read(size)
+                #else: # PROXY CASE
+                #    chunk = self.obj_data.next()
+        except Timeout:
+            self.close()
+            raise
+        except Exception:
+            self.close()
+            raise
+        return chunk
+
+    def next(self, size=64 * 1024):
+        if len(self.buf) < size:
+            self.buf += self.read_with_timeout(size - len(self.buf))
+            
+            if self.buf == b'':
+                self.close()
+                raise StopIteration('Stopped iterator exp')
+
+        if len(self.buf) > size:
+            data = self.buf[:size]
+            self.buf = self.buf[size:]
+        else:
+            data = self.buf
+            self.buf = b''
+        
+        return data
+
+    def _close_check(self):
+        if self.closed:
+            raise ValueError('I/O operation on closed file')
+
+    def read(self, size=64 * 1024):
+        self._close_check()
+        return self.next(size)
+
+    def readline(self, size=-1):
+        self._close_check()
+
+        # read data into self.buf if there is not enough data
+        while b'\n' not in self.buf and \
+              (size < 0 or len(self.buf) < size):
+            if size < 0:
+                chunk = self.read()
+            else:
+                chunk = self.read(size - len(self.buf))
+            if not chunk:
+                break
+            self.buf += chunk
+
+        # Retrieve one line from buf
+        data, sep, rest = self.buf.partition(b'\n')
+        data += sep
+        self.buf = rest
+
+        # cut out size from retrieved line
+        if size >= 0 and len(data) > size:
+            self.buf = data[size:] + self.buf
+            data = data[:size]
+
+        return data
+
+    def readlines(self, sizehint=-1):
+        self._close_check()
+        lines = []
+        try:
+            while True:
+                line = self.readline(sizehint)
+                if not line:
+                    break
+                lines.append(line)
+                if sizehint >= 0:
+                    sizehint -= len(line)
+                    if sizehint <= 0:
+                        break
+        except StopIteration:
+            pass
+        return lines
+
+    def close(self):
+        if self.closed:
+            return
+        self.closed = True
+        print "---> Closing BW-DIFF Pipe <---"
         self.obj_data.close()
 
     def __del__(self):
         self.close()
 
 
-class Singleton:
-    """
-    A non-thread-safe helper class to ease implementing singletons.
-    This should be used as a decorator -- not a metaclass -- to the
-    class that should be a singleton.
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):  # @NoSelf
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-    The decorated class can define one `__init__` function that
-    takes only the `self` argument. Other than that, there are
-    no restrictions that apply to the decorated class.
-
-    To get the singleton instance, use the `Instance` method. Trying
-    to use `__call__` will result in a `TypeError` being raised.
-
-    Limitations: The decorated class cannot be inherited from.
-    """
-
-    def __init__(self, decorated):
-        self._decorated = decorated
-
-    def Instance(self, **args):
-        """
-        Returns the singleton instance. Upon its first call, it creates a
-        new instance of the decorated class and calls its `__init__` method.
-        On all subsequent calls, the already created instance is returned.
-
-        """
-        logger = args['logger']
-        try:
-            if self._instance:
-                logger.info("SDS Bandwidth Differentiation - Singleton "
-                            "instance of bandwidth control already created")
-                return self._instance
-        except AttributeError:
-            logger.info("SDS Bandwidth Differentiation - Creating "
-                        "singletoninstance of bandwidth control")
-            self._instance = self._decorated(**args)
-            return self._instance
-
-    def __call__(self):
-        raise TypeError('Singletons must be accessed through `Instance()`.')
-
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._decorated)
-
-
-@Singleton
 class BandwidthControl(object):
-
+    __metaclass__ = Singleton
+    
     def __init__(self, global_conf, filter_conf, logger):
         self.tenant_request_thread = {}
         self.tenant_response_thread = {}
@@ -493,7 +681,6 @@ class BandwidthControl(object):
         self._start_monitoring_producer()
         self._start_assignments_consumer()
 
-
     def execute(self, req_resp, app_iter, requets_data):     
         if isinstance(req_resp, Response):
             app_iter = self._register_response(requets_data['account'], req_resp, app_iter)
@@ -505,10 +692,11 @@ class BandwidthControl(object):
        
     def _register_request(self, tenant, request, app_iter = None):
         r, w = os.pipe()
-        out_reader = os.fdopen(r,'r') 
+        #out_reader = os.fdopen(r,'r') 
         write_pipe = os.fdopen(w,'w')
         
         if app_iter:
+            # Never enter here
             read_pipe = app_iter.read
         else:
             read_pipe = request.environ['wsgi.input'].read
@@ -533,17 +721,18 @@ class BandwidthControl(object):
           
         thr.add_stream_to_tenant(write_pipe, read_pipe, policy, device)
         
-        return IterLike(out_reader, 10)
+        return IterLike(r, 10)
     
     def _register_response(self, tenant, response, app_iter = None):
         r, w = os.pipe()
-        out_reader = os.fdopen(r,'r')
+        #out_reader = os.fdopen(r,'r')
         write_pipe = os.fdopen(w,'w')
         
         if app_iter:
             read_pipe = app_iter
+ 
         else:
-            if self.server == 'proxy': 
+            if self.server == 'proxy':
                 read_pipe = response.app_iter
             else: 
                 read_pipe = response.app_iter._fp.read
@@ -563,8 +752,8 @@ class BandwidthControl(object):
         else: thr = self.tenant_response_thread[tenant]
 
         thr.add_stream_to_tenant(write_pipe, read_pipe, policy, device)
- 
-        return IterLike(out_reader, 10)
+        
+        return IterLike(r, 10)
         
     def register_ssync(self, request):
         r, w = os.pipe()
@@ -651,7 +840,7 @@ class BandwidthControl(object):
                 if not threads[tenant].alive:
                     self.log.info("SDS Bandwidth Differentiation - Killing "
                                   "thread "+tenant)
-                    del threads[tenant]   
+                    del threads[tenant]
      
     def _get_monitoring_info(self, threads, interval):
         '''Dictionary with bw estimates'''
@@ -709,8 +898,9 @@ class BandwidthControl(object):
         channel.exchange_declare(exchange=consumer_tag, type='topic')
         channel.queue_bind(exchange=consumer_tag, queue=queue_bw, routing_key=routing_key)
         channel.basic_consume(self._bw_assignations, queue=queue_bw, no_ack=True)
-        th_assignation = Thread(target=channel.start_consuming)
-        th_assignation.start()
+        
+        thbw_assignation = Thread(target=channel.start_consuming)
+        thbw_assignation.start()
         
     def _bw_assignations(self, ch, m, properties, body):
         _, account, method, policy, device, bw = body.split('/')
@@ -733,4 +923,3 @@ class BandwidthControl(object):
                 self.ssync_thread[account].update_bw_limits(float(bw))
             except KeyError:
                 pass
-        
