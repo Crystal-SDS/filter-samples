40a5c7efd48e84b48647d4462960e11620407a7e
JosepSampe <jsampe@gmail.com>
2017-10-19 20:33:01 +0200

    Modified native filters

--- Native_tag/crystal_tag.py
+++ Native_tag/crystal_tag.py
@@ -1,44 +1,56 @@
-from crystal_filter_middleware.filters.abstract_filter import AbstractFilter
-import re
-
 """
 MetadataEnhancer Filter.
 This filter adds specific tags based on file name patterns.
 """
+from swift.common.utils import get_logger
+from swift.common.swob import wsgify
+from swift.common.utils import register_swift_info
+import re
 
 
-class MetadataEnhancer(AbstractFilter):
+class MetadataEnhancer(object):
 
-    def __init__(self, global_conf, filter_conf, logger):
-        super(MetadataEnhancer, self).__init__(global_conf, filter_conf, logger)
+    def __init__(self, app, conf):
+        self.app = app
+        self.conf = conf
+        self.logger = get_logger(self.conf, log_route='metadata_filter')
+        self.filter_data = self.conf['filter_data']
+        self.parameters = self.filter_data['params']
 
-    def _apply_filter(self, req_resp, data_iter, parameters):
-        method = req_resp.environ['REQUEST_METHOD']
+        self.register_info()
 
-        if method == 'PUT':
-            return self._put_object(req_resp, data_iter, parameters)
+    def register_info(self):
+        register_swift_info('metadata_filter')
+
+    @wsgify
+    def __call__(self, req):
+        if req.method == 'PUT':
+            regex = None
+            tag = None
+
+            if 'regex' in self.parameters:
+                regex = self.parameters['regex']
+            if 'tag' in self.parameters:
+                tag = self.parameters['tag']
+
+            if regex and tag:
+                _, _, _, obj = req.split_path(4, 4, rest_with_last=True)
+                pattern = re.compile(regex)
+                if pattern.search(obj):
+                    header, value = tag.split(':')
+                    self.logger.info('Tagging Filter - Adding '+tag+' tag to the object')
+                    req.headers['X-Object-Meta-'+header] = value
+            else:
+                self.logger.error('Tagging Filter - No regex or tag provided')
+            return req.get_response(self.app)
         else:
-            return data_iter
+            return req.get_response(self.app)
 
-    def _put_object(self, request, data_iter, parameters):
-        regex = None
-        tag = None
 
-        if 'regex' in parameters:
-            regex = parameters['regex']
-        if 'tag' in parameters:
-            tag = parameters['tag']
+def filter_factory(global_conf, **local_conf):
+    conf = global_conf.copy()
+    conf.update(local_conf)
 
-        if regex and tag:
-            _, _, _, obj = request.split_path(4, 4, rest_with_last=True)
-            pattern = re.compile(regex)
-            if pattern.search(obj):
-                header, value = tag.split(':')
-                self.logger.info('Tagging Filter - Adding '+value+' tag to the object')
-                request.headers['X-Object-Meta-'+header] = value
-        else:
-            self.logger.error('Tagging Filter - No regex or tag provided')
-
-        # This filter does not modifies the original data, so we return
-        # the original data_iter.
-        return request.environ['wsgi.input']
+    def metadata_enhancer_filter(app):
+        return MetadataEnhancer(app, conf)
+    return metadata_enhancer_filter
