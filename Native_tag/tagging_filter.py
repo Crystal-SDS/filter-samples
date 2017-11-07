9bd4bffcb83c655068635980ec96176316304dfc
JosepSampe <jsampe@gmail.com>
2017-11-07 14:27:05 +0100

    tagging filter

--- Native_tag/tagging_filter.py
+++ Native_tag/tagging_filter.py
@@ -1,5 +1,5 @@
 """
-MetadataEnhancer Filter.
+Tagging Filter.
 This filter adds specific tags based on file name patterns.
 """
 from swift.common.utils import get_logger
@@ -13,14 +13,14 @@ class TaggingFilter(object):
     def __init__(self, app, conf):
         self.app = app
         self.conf = conf
-        self.logger = get_logger(self.conf, log_route='metadata_filter')
+        self.logger = get_logger(self.conf, log_route='tagging_filter')
         self.filter_data = self.conf['filter_data']
         self.parameters = self.filter_data['params']
 
         self.register_info()
 
     def register_info(self):
-        register_swift_info('metadata_filter')
+        register_swift_info('tagging_filter')
 
     @wsgify
     def __call__(self, req):
@@ -37,9 +37,10 @@ class TaggingFilter(object):
                 _, _, _, obj = req.split_path(4, 4, rest_with_last=True)
                 pattern = re.compile(regex)
                 if pattern.search(obj):
-                    header, value = tag.split(':')
+                    key, value = tag.split(':')
                     self.logger.info('Tagging Filter - Adding '+tag+' tag to the object')
-                    req.headers['X-Object-Meta-'+header] = value
+                    req.headers['X-Object-Meta-'+key] = value
+                    req.headers['X-Object-Sysmeta-'+key] = value
             else:
                 self.logger.error('Tagging Filter - No regex or tag provided')
             return req.get_response(self.app)
